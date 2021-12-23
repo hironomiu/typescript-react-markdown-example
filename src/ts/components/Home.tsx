280e6c38bfce59f2044ff5eafc43c1b5ec392105
hironomiu <hironomiu@gmail.com>
2021-12-23 17:51:21 +0900

    commited

--- src/ts/components/Home.tsx
+++ src/ts/components/Home.tsx
@@ -1,38 +1,26 @@
-import React, { FC } from 'react'
+import { FC, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
 
 const Home: FC = () => {
-  const markdown = `
-# hoge
-aiueo
+  const [markdown, setMarkdown] = useState('')
 
-## fuga
-kakiku
-
-### piyo
-sasisuseso 
-
-~~~js:hoge.js
-hogehoge
-~~~
-
-- hoho
-  - fufu
-
-aaaaaaaaaaaaaaaaaaaaaaaaaaa
----
-
-**aaa**
-  `
   return (
     <>
-      <div className="flex flex-col item-center justify-center">
-        <div className="markdown whitespace-pre-wrap px-10 text-start">
-          <ReactMarkdown plugins={[gfm]}>{markdown}</ReactMarkdown>
+      <div className="flex flex-row pt-10">
+        <div className="mx-10 w-600">
+          <textarea
+            className="border cursor-w-resize text-black caret-pink-500 focus:caret-red-500"
+            name=""
+            id=""
+            rows={20}
+            cols={59}
+            onChange={(e) => setMarkdown(e.target.value)}
+            value={markdown}
+          />
         </div>
-        <div className=" text-center">
-          <h1>not markdown</h1>
+        <div className="markdown whitespace-pre-wrap break-words w-600 text-start border rounded">
+          <ReactMarkdown plugins={[gfm]}>{markdown}</ReactMarkdown>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        600: '600px',
+      },
+    },
   },
   variants: {
     extend: {},
