c196f328b158d53539b035e8688061a449983c79
hironomiu <hironomiu@gmail.com>
2022-02-07 17:36:38 +0900

    commited

--- src/ts/components/Home.tsx
+++ src/ts/components/Home.tsx
@@ -10,17 +10,20 @@ const Home: FC = () => {
       <div className="flex flex-row pt-10">
         <div className="mx-10 h-600 w-600">
           <textarea
-            className="border cursor-w-resize text-black caret-pink-500 focus:caret-red-500"
+            className="border caret-red-500  pl-2 pt-2"
             name=""
             id=""
             rows={25}
             cols={59}
             onChange={(e) => setMarkdown(e.target.value)}
             value={markdown}
+            // style={{ caretColor: 'red' }}
           />
         </div>
         <div className="markdown leading-none tracking-normal whitespace-pre-wrap overflow-scroll break-words h-600 w-600 text-start border rounded">
-          <ReactMarkdown plugins={[gfm]}>{markdown}</ReactMarkdown>
+          <ReactMarkdown className="pl-2" plugins={[gfm]}>
+            {markdown}
+          </ReactMarkdown>
         </div>
       </div>
     </>
